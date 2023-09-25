5b0f56c246d0fe732d31a8717220af57c2c8b685
NovusX-Dev <novusxdev@outlook.com>
2023-09-25 06:16:36 -0300

    - proper readme

---
+++ Jenga Class Project/Assets/ReadMe.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadMe
+{
+    //Thank you for this opportunity.
+    
+    // Here is a link to the project's github repo:
+    //
+    // https://github.com/NovusX-Dev/jenga-class-project
+}
--- Jenga Class Project/ReadMe.txt
+++
@@ -1,5 +0,0 @@
-//Thank you for this oppotrunity.
-
-Here is a link to the project's github repo:
-
-https://github.com/NovusX-Dev/jenga-class-project
